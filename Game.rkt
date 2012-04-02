c08b94fad9b0cdfa2d2a7e4aa84ffe55b7ad69c8
Karl Linderhed <karli315@student.liu.se>
2012-04-02 12:01:32 +0200

    added texture handling with wall and floor textures

--- Game.rkt
+++ Game.rkt
@@ -38,18 +38,68 @@
   (new timer% (interval 500) (notify-callback tick))
   
   (glDisable GL_DEPTH_TEST)
+  
+  (set! texture-list (glGenTextures 9))
   (glEnable GL_TEXTURE_2D)
-  (define walltex (image->gl-vector "wall.png"))
+  (define floortex (image->gl-vector "images/floortile.png"))
+  (define walltexleft (image->gl-vector "images/walltileleft.png"))
+  (define walltexright (image->gl-vector "images/walltileright.png"))
+  (define walltextop (image->gl-vector "images/walltiletop.png"))
+  (define walltexbot (image->gl-vector "images/walltilebottom.png"))
+  (define walltexspecleft (image->gl-vector "images/specwallleft.png"))
+  (define walltexspecright (image->gl-vector "images/specwallright.png"))
+  (define walltexcornerbotl (image->gl-vector "images/wallcornerbotleft.png"))
+  (define walltexcornerbotr (image->gl-vector "images/wallcornerbotright.png"))
+  
   (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 0))
   (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
   (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref floortex 0) (list-ref floortex 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref floortex 2))
   
-  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltex 0) (list-ref walltex 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltex 2))
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 1))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexleft 0) (list-ref walltexleft 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexleft 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 2))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexright 0) (list-ref walltexright 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexright 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 3))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltextop 0) (list-ref walltextop 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltextop 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 4))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexbot 0) (list-ref walltexbot 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexbot 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 5))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexspecleft 0) (list-ref walltexspecleft 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexspecleft 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 6))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexspecright 0) (list-ref walltexspecright 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexspecright 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 7))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexcornerbotl 0) (list-ref walltexcornerbotl 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexcornerbotl 2))
+  
+  (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 8))
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+  (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+  (glTexImage2D GL_TEXTURE_2D 0 3 (list-ref walltexcornerbotr 0) (list-ref walltexcornerbotr 1) 0 GL_RGB GL_UNSIGNED_BYTE (list-ref walltexcornerbotr 2))
   
   (glEnable GL_BLEND)
   (glBlendFunc GL_SRC_ALPHA GL_ONE_MINUS_SRC_ALPHA)
   
-  (glClearColor 1 1 1 1)
+  (glClearColor 0 0 0 1)
   (glViewport 0 0 (send glcanvas get-width) (send glcanvas get-height))
   
   (glMatrixMode GL_PROJECTION)
@@ -110,7 +160,7 @@
   (glOrtho 0 (send glcanvas get-width) (send glcanvas get-height) 0 -1 1)
   
   (let ((current-map (send world get-current-map))
-        (tile-width 64)
+        (tile-width 32)
         (x 0))
     (define (xloop)
       (when (< x (send current-map get-sizex))
@@ -122,14 +172,27 @@
               (glTranslatef (* x tile-width) (* y tile-width) 0)
               (glMatrixMode GL_PROJECTION)
               (glPushMatrix)
-              (glBegin GL_TRIANGLE_STRIP)
+              (glColor3f 1 1 1)
               (case (send (send current-map gettile x y) get-type)
-                ((#\w) (glColor3f 1 0 0))
-                ((#\f) (glColor3f 0 1 0))
-                ((#\h) (glColor3f 0 0 1)))
+                
+                ((#\f) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 0)))
+                ((#\l) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 1)))
+                ((#\r) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 2)))       
+                ((#\t) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 3)))
+                ((#\b) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 4)))
+                ((#\1) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 5)))
+                ((#\2) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 6)))
+                ((#\3) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 7)))
+                ((#\4) (glBindTexture GL_TEXTURE_2D (gl-vector-ref texture-list 8)))
+                ((#\h) (glColor3f 0 0 0)))
+              (glBegin GL_TRIANGLE_STRIP)
+              (glTexCoord2i 0 0)
               (glVertex2i 0 0)
+              (glTexCoord2i 1 0)
               (glVertex2i tile-width 0)
+              (glTexCoord2i 0 1)
               (glVertex2i 0 tile-width)
+              (glTexCoord2i 1 1)
               (glVertex2i tile-width tile-width)
               (glEnd)
               (glPopMatrix)
@@ -149,15 +212,17 @@
 
 
 
-(define (tick)
-  (send glcanvas refresh))
+(define tick
+  (let ((ticks 0))
+    (lambda ()
+      (send glcanvas refresh))))
 
 ;----------------------------------------------------------------------------
 ;                           Object declarations
 ;----------------------------------------------------------------------------
 
 (define backgrounds '())
-
+(define texture-list #f)
 (define mapplista (list (Load&Create 'test-room "Loadtest.txt")))
 (define world (new World%
                    (maplist mapplista) 
