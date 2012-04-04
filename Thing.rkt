8341fa3a9ccc498cb83008fb3e6feeed2d89c66d
Karl Linderhed <karli315@student.liu.se>
2012-04-04 17:01:29 +0200

    started on the things

--- Thing.rkt
+++ Thing.rkt
@@ -1,11 +1,28 @@
 (define Thing%
   (class Agent%
     (super-new)
-    (inherit-field xpos ypos triggerlist)
+    (inherit-field xpos 
+                   ypos 
+                   gridx
+                   gridy
+                   triggerlist)
     (define/public (use) "not implemented yet")
-    (define/public (setx newx)
-      (set! xpos newx))
+    
     (define/public (getx)
+      gridx)
+    (define/public (gety)
+      gridy)
+    
+    (define/public (get-xpos)
       xpos)
+    (define/public (get-ypos)
+      ypos)
+    
+    (define/public (get-triggers)
+      triggerlist)
+    
+    (define/public (add-trigger! trigger)
+      (set! triggerlist (cons trigger triggerlist)))
     (define/public (render)
-      "not implemented yet")))
+      "not implemented yet"
+      )))
